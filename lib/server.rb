51f6f13a849ab201fedeb14651535f2840b35555
Sam Landfried <samlandfried@gmail.com>
2017-02-10 20:32:06 -0700

    Complete iteration 0. Strange attr_accessor behavior

--- lib/server.rb
+++ lib/server.rb
@@ -1,34 +1,54 @@
 require 'socket'
+require 'pry'
 
 class Server
 
-  attr_reader :server
+  # attr's not working as I would expect... why?
 
-  attr_accessor :counter
+  attr_accessor :server, :socket, :counter, :output, :headers
 
   def initialize port
     @server = TCPServer.new port
+    @headers = ["headers"]
+    @output = "output"
     @counter = 0
   end
 
   def listen
     loop do
-      # Thread.start(server.accept) do |client|
-      client = server.accept
-      increment_counter
-      client.puts "Hello!"
-      client.puts "Time is #{Time.now}"
-      client.puts "#{counter}"
-      client.close
-      # end
+      puts "Listening for request..."
+      @socket = server.accept
+      @counter += 1 # <------- I can't access counter as an attr_accessor
+      request = receive_request
+      puts "Got this request:\n#{request}"
+      puts "Writing response..."
+      write_response
+      socket.puts headers # <-- But I can access headers and output here via attr_accessor, but not in .write_response
+      socket.puts output
+      puts "Wrote this response:\n#{output}"
+      puts "With these headers:\n#{headers}"
+      socket.close
     end
   end
 
-  def increment_counter
-    counter += 1
+  def receive_request
+    request_lines = []
+    while line = socket.gets and !line.chomp.empty?
+      puts "Reading request..."
+      request_lines << line.chomp
+    end
+    request_lines.join("\n")
   end
 
-
+  def write_response # attr_accessor
+    response = "<pre>Hello, World! (#{counter})</pre>" # <---------- I can access counter via attr_accessor
+    @output = "<html><head></head><body>#{response}</body></html>" # But not headers and output
+    @headers = ["http/1.1 200 ok",
+                "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
+                "server: ruby",
+                "content-type: text/html; charset=iso-8859-1",
+                "content-length: #{output.length}\r\n\r\n"].join("\r\n")
+  end
 end
 
 Server.new(9292).listen
