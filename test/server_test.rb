a459487052eb6a273b3c939ca22432ca2dac419a
Sam Landfried <samlandfried@gmail.com>
2017-02-11 15:04:44 -0700

    Faraday works... Sorta

--- test/server_test.rb
+++ test/server_test.rb
@@ -3,20 +3,21 @@ require 'minitest/emoji'
 require 'faraday'
 require 'pry'
 
-# require './lib/server'
+require './lib/server'
 
 
 class ServerTest < Minitest::Spec
   describe 'Iteration 0' do
 
+    attr_reader :server
+
     before do
       # puts response
+      server = Server.new(2525).listen
     end
 
     it "should listen on port 9292" do
-      response = Faraday.get("http://127.0.0.1:2000")
-      # response = conn.get
-      puts response
+      ->{Faraday.get("http://127.0.0.1:2525")}.must_throw ConnectionFailed
     end
 
     it "should respond to HTTP requests" do
