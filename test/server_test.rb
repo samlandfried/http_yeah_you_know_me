a92b2d4a5fedfcf6479bdc5053bacfb367723c21
Sam Landfried <samlandfried@gmail.com>
2017-02-13 15:05:31 -0700

    Pass root request test

--- test/server_test.rb
+++ test/server_test.rb
@@ -5,32 +5,67 @@ require 'pry'
 
 require './lib/server'
 
-
 class ServerTest < Minitest::Spec
-  describe 'Iteration 0' do
 
-    attr_reader :server
+
+
+  describe 'When it starts' do
+
+    attr_reader :response, :server
 
     before do
-      # puts response
-      server = Server.new(2525).listen
+      @server = Server.new(123)
+      @response = Faraday.get("http://127.0.0.1:9292")
     end
 
-    it "should listen on port 9292" do
-      ->{Faraday.get("http://127.0.0.1:2525")}.must_throw ConnectionFailed
+    it "should listen to and respond on port 9292" do
+      ->{Faraday.get("http://127.0.0.1:929")}.must_raise Faraday::ConnectionFailed
+      response.status.must_equal 200
     end
 
-    it "should respond to HTTP requests" do
-
+    it "should be ready to count requests" do
+      server.counts[:total].must_equal 0
+      server.counts[:hello].must_equal 0
     end
 
-    it "displays 'Hello, World!'" do
-    end
+  end
 
-    it "counts the number of server requests" do
-    end
 
-    after do
+  describe "When a request is made to /" do
+
+    attr_reader :response
+
+
+    it "should serve the request info" do
+      # binding.pry
+      response.body.must_include("Verb: GET\nPath: /\nParams: {}\nProtocol: HTTP/1.1\nUser-agent: Faraday v0.11.0\nAccept-encoding: gzip;q=1.0,deflate;q=0.6,identity;q=0.3\nAccept: */*\nConnection: close\nHost: 127.0.0.1\nPort: 9292")
     end
   end
+
+  describe "When a request is made to /hello" do
+    before do
+      @response = Faraday.get("http://127.0.0.1:9292/hello")
+    end
+
+    it "should "
+  end
+
+  describe "When a request is made to /datetime" do
+  end
+
+  describe "When a request is made to /start_game" do
+  end
+
+  describe "When a request is made to /game" do
+  end
+
+  describe "When a request is made to /force_error" do
+  end
+
+  describe "When a request is made to root" do
+  end
+
+  describe "When a request is made to root" do
+  end
+
 end
